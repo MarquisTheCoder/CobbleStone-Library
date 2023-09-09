6a017712f65f2859ca37cae39e05a296e52adfa3
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-09-09 05:29:49 -0500

    auto save script Sat Sep  9 05:29:49 CDT 2023

--- src/lib/components/homepage/Content.svelte
+++ src/lib/components/homepage/Content.svelte
@@ -1,8 +1,18 @@
 <script>
 	import ScrollProgress from '@pluginjs/scroll-progress';
 	import '@pluginjs/scroll-progress/dist/scroll-progress.css';
-
-	ScrollProgress.of(document.querySelector('.element'), options);
+	let options = {
+		size: 5,
+		color: '#000000',
+		opacity: 1,
+		custom: true,
+		appendTo: 'body',
+		position: 'top-left',
+		templates: {}
+	};
+	ScrollProgress.of(document.querySelector('.library'), options);
 </script>
 
-<section id="content" class="h-[40rem] w-screen bg-emerald-500/50">sd</section>
+<section id="content" class="h-[40rem] w-screen bg-emerald-500/50">
+	<div class="library" />
+</section>
