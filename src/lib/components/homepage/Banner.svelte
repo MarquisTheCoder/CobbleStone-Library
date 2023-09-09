27a723a763b147c5876bd887bf4e0832bc8d25e4
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-09-09 15:39:48 -0500

    auto save script Sat Sep  9 15:39:48 CDT 2023

--- src/lib/components/homepage/Banner.svelte
+++ src/lib/components/homepage/Banner.svelte
@@ -1,8 +1,9 @@
 <section
-	class="w-full h-[40rem] md:h-[30rem] px-8 pt-20 md:px-28 flex flex-row justify-center md:items-center"
+	class="w-full h-fit md:h-[30rem] px-8 pt-20 md:px-28 flex flex-row justify-center md:items-center"
 >
-	<h1 class="text-[4rem] md:text-[4rem] font-bold text-black/70 w-full ">
-		If you have <a href="#js-toc" class="text-emerald-500 hover:text-emerald-800 cursor-pointer">knowledge</a>, let
-		others light their candles in it.
+	<h1 class="text-[4rem] md:text-[4rem] font-bold text-black/70 w-full mb-1">
+		If you have <a href="#js-toc" class="text-emerald-500 hover:text-emerald-800 cursor-pointer"
+			>knowledge</a
+		>, let others light their candles in it.
 	</h1>
 </section>
