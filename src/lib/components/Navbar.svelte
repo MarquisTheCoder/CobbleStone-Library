0cae07619744d2b48c426b89156dd322d2628a52
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-09-09 17:26:52 -0500

    auto save script Sat Sep  9 17:26:52 CDT 2023

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,8 +1,9 @@
 <nav class="flex flex-col gap-4 md:flex-row md:justify-between md:px-24 pt-28 items-center">
 	<div class="md:w-[50%] flex items-center">
-		<site class=" text-[2rem] text-center md:text-[3rem] font-thin opacity-75 w-full flex flex-row items-center gap-3"
+		<site
+			class=" text-[2rem] text-center md:text-[3rem] font-thin opacity-75 w-full flex flex-row items-center gap-3"
 			>Cobblestone Library
-			<img src="/imgs/flame.svg" alt="" class="flex-shrink-0 w-10 h-10"  />
+			<img src="/imgs/flame.svg" alt="" class="flex-shrink-0 w-10 h-10" />
 		</site>
 	</div>
 	<div class="md:w-[50%] flex flex-row items-center justify-end gap-4">
@@ -10,13 +11,15 @@
 			<a href="https://github.com" class="flex-shrink-0">
 				<img src="/imgs/github.svg" alt="" class="flex-shrink-0 w-5 h-15" />
 			</a>
-			<a href="https://github.com/MarquisTheCoder/cobblestone-library" class="font-thin hover:text-emerald-600"
-				>@CobblestoneRepository</a
+			<a
+				href="https://github.com/MarquisTheCoder/cobblestone-library"
+				class="font-thin hover:text-emerald-600"
+				target="_blank">@CobblestoneRepository</a
 			>
 		</div>
 		<div class="flex flex-row items-center">
 			<a href="https://discord.gg/nTska3D2dn" class="flex-shrink-0">
-				<img src="/imgs/discord.svg" alt="" class="flex-shrink-0 w-7 " />
+				<img src="/imgs/discord.svg" alt="" class="flex-shrink-0 w-7" />
 			</a>
 		</div>
 	</div>
