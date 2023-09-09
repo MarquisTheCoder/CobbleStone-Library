cb780a089d3fd92c7daf7a5eaee5e21fcbca2b47
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-09-09 11:00:32 -0500

    auto save script Sat Sep  9 11:00:32 CDT 2023

--- src/lib/components/homepage/Content.svelte
+++ src/lib/components/homepage/Content.svelte
@@ -21,6 +21,23 @@
 	researchers and students
  -->
 <section id="content" class="h-[40rem] w-screend px-6 md:px-32 mt-24">
+	<div class="mb-20">
+		<p class="font-thin">
+			The <a href="https://discord.gg/nTska3D2dn" class="font-bold text-black/80">Cobblestone Library Discord Server</a> has emerged as an invaluable
+			online education resource, fostering a close-knit community of learners for several years. Its
+			unique blend of curated content, interactive discussions, and passionate members has transformed
+			it into a hidden gem in the world of online education. At its core, Cobblestone Library serves
+			as a digital repository of knowledge, with an extensive collection of resources covering a wide
+			array of subjects. What sets it apart is its active, engaged membership. Within this tight-knit
+			group, learners and educators from diverse backgrounds come together to exchange ideas, offer support,
+			and facilitate stimulating conversations. This server's dedication to fostering a collaborative
+			learning environment, where individuals of all skill levels can thrive, has cultivated a sense
+			of camaraderie and intellectual growth. By sharing the riches of the Cobblestone Library Discord
+			server with a broader audience, we hope to extend this exceptional online education resource to
+			more individuals, empowering them to embark on their own educational journeys within this vibrant,
+			welcoming community.
+		</p>
+	</div>
 	<div
 		class="library h-[10rem] w-screen flex flex-col md:flex-row gap-6 p-4 font-thin text-[1.4rem] relative"
 	>
