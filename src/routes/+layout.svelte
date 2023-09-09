c2d5369c02c91312a761120d23baf1fd7834616d
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-09-09 16:18:43 -0500

    auto save script Sat Sep  9 16:18:43 CDT 2023

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,36 @@
 </script>
 
 <svelte:head>
+	<meta charset="utf-8" />
+	<meta
+		name="description"
+		content="Website made to spread knowledge based on popular discord server, Author: MarquisTheCoder, Category: Technology, Books, Pdfs"
+	/>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+
+	<meta property="og:type" content="article" />
+
+	<meta property="og:title" content="Cobblestone Library" />
+
+	<meta property="og:description" content="Website made to spread knowledge based on popular discord server, Author: MarquisTheCoder, Category: Technology, Books, Pdfs" />
+
+	<meta property="og:image" content="https://pbs.twimg.com/media/F5mAtMZXoAE2oX0?format=jpg&name=4096x4096" />
+
+	<meta property="og:url" content="https://cobblestone-library.onrender.com/" />
+
+	<meta property="og:site_name" content="Cobblestone Library" />
+
+	<meta name="twitter:title" content="Cobblestone Liobrary" />
+
+	<meta name="twitter:description" content="Website made to spread knowledge based on popular discord server, Author: MarquisTheCoder, Category: Technology, Books, Pdfs" />
+
+	<meta name="twitter:image" content="https://pbs.twimg.com/media/F5mAtMZXoAE2oX0?format=jpg&name=4096x4096" />
+
+	<meta name="twitter:site" content="https://cobblestone-library.onrender.com/" />
+
+	<meta name="twitter:creator" content="@MarquisTheCoder" />
+	<title>Cobblestone Library | bastion of freedom knowledge</title>
+
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/tocbot/4.18.2/tocbot.min.js"></script>
 </svelte:head>
 <slot />
