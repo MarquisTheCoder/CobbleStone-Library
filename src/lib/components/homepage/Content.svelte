1a10821d9233ec8202bfa8f0086855f3930d8a4c
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-09-09 05:44:43 -0500

    auto save script Sat Sep  9 05:44:43 CDT 2023

--- src/lib/components/homepage/Content.svelte
+++ src/lib/components/homepage/Content.svelte
@@ -1,18 +1,3 @@
-<script>
-	import ScrollProgress from '@pluginjs/scroll-progress';
-	import '@pluginjs/scroll-progress/dist/scroll-progress.css';
-	let options = {
-		size: 5,
-		color: '#000000',
-		opacity: 1,
-		custom: true,
-		appendTo: 'body',
-		position: 'top-left',
-		templates: {}
-	};
-	ScrollProgress.of(document.querySelector('.library'), options);
-</script>
-
-<section id="content" class="h-[40rem] w-screen bg-emerald-500/50">
-	<div class="library" />
+<section id="content" class="h-[40rem] w-screen bg-emerald-500/50 px-32">
+	<div class="library h-full w-full bg-purple" />
 </section>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script>
-  import "../app.css";
+	import '../app.css';
 </script>
 
 <svelte:head>
-  <script src="https://unpkg.com/@pluginjs/scroll-progress/dist/scroll-progress.js"></script>
-<link rel="stylesheet" href="https://unpkg.com/@pluginjs/scroll-progress/dist/scroll-progress.css">
+	<script src="https://unpkg.com/@pluginjs/scroll-progress/dist/scroll-progress.js"></script>
+	<link
+		rel="stylesheet"
+		href="https://unpkg.com/@pluginjs/scroll-progress/dist/scroll-progress.css"
+	/>
+	<script src="/static/scripts/scroll.js"></script>
 </svelte:head>
 <slot />
