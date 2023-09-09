e16e4afadacc18e0a52f82170c510f077e0866f5
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-09-09 10:39:58 -0500

    auto save script Sat Sep  9 10:39:58 CDT 2023

--- src/lib/components/homepage/BookLink.svelte
+++ src/lib/components/homepage/BookLink.svelte
@@ -3,4 +3,8 @@
 	export let name = '';
 </script>
 
-<li><a {href} class="chapter-link text-[#0c8981] font-[700]"> <span class="text-2xl text-black font-normal">-   </span>{name}</a></li>
+<li>
+	<a {href} target="_blank" class="chapter-link text-[#0c8981] font-[700]">
+		<span class="text-2xl text-black font-normal">- </span>{name}</a
+	>
+</li>
