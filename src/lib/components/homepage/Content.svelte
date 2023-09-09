2330d92c0c029b042054db9f17da77cb3d64e320
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-09-09 16:55:49 -0500

    auto save script Sat Sep  9 16:55:49 CDT 2023

--- src/lib/components/homepage/Content.svelte
+++ src/lib/components/homepage/Content.svelte
@@ -23,19 +23,18 @@
 <section id="content" class="h-[40rem] w-screend px-6 md:px-32">
 	<div class="mb-20 mt-10 md:mt-24">
 		<p class="font-thin">
-			The <a href="https://discord.gg/nTska3D2dn" class="font-bold text-black/80"
-				>Cobblestone Library Discord Server</a
-			> has emerged as an invaluable online education resource, fostering a close-knit community of learners
-			for several years. Its unique blend of curated content, interactive discussions, and passionate
-			members has transformed it into a hidden gem in the world of online education. At its core, Cobblestone
-			Library serves as a digital repository of knowledge, with an extensive collection of resources
-			covering a wide array of subjects. What sets it apart is its active, engaged membership. Within
-			this tight-knit group, learners and educators from diverse backgrounds come together to exchange
-			ideas, offer support, and facilitate stimulating conversations. This server's dedication to fostering
-			a collaborative learning environment, where individuals of all skill levels can thrive, has cultivated
-			a sense of camaraderie and intellectual growth. By sharing the riches of the Cobblestone Library
-			Discord server with a broader audience, we hope to extend this exceptional online education resource
-			to more individuals, empowering them to embark on their own educational journeys within this vibrant,
+			The <a href="https://discord.gg/nTska3D2dn" class="font-bold text-black/80">Cobblestone Library Discord Server</a> has emerged as an invaluable
+			online education resource, fostering a close-knit community of learners for several years. Its
+			unique blend of curated content, interactive discussions, and passionate members has transformed
+			it into a hidden gem in the world of online education. At its core, Cobblestone Library serves
+			as a digital repository of knowledge, with an extensive collection of resources covering a wide
+			array of subjects. What sets it apart is its active, engaged membership. Within this tight-knit
+			group, learners and educators from diverse backgrounds come together to exchange ideas, offer support,
+			and facilitate stimulating conversations. This server's dedication to fostering a collaborative
+			learning environment, where individuals of all skill levels can thrive, has cultivated a sense
+			of camaraderie and intellectual growth. By sharing the riches of the Cobblestone Library Discord
+			server with a broader audience, we hope to extend this exceptional online education resource to
+			more individuals, empowering them to embark on their own educational journeys within this vibrant,
 			welcoming community.
 		</p>
 	</div>
